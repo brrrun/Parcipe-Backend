bb6ada9134ccda124021426f7507f7a2055b82a3
brrrun <brunomsantosmendes@gmail.com>
2024-02-20 15:22:27 +0000

    Add user PUT route plus user model changes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,18 +2,32 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-const User = ("../models/User.model")
+const User = require("../models/User.model");
 
-    // Creates a new Profile / Sign Up
-    router
-    .post("/profile/new", (req, res)=>{
-        const {username, email, password} = req.body;
+// Fetches the user Profile
+router.get("/profile/:_id", (req, res) => {
+    const { _id } = req.params;
+    User.findById(_id)
+        .then((user) => res.json(user))
+        .catch((error) => res.status(500).json({ error: "Failed to fetch profile", details: error }));
+});
 
-    User
-    .create({username, email, password})
-    .then((newUser) => res.json(newUser))
-    .catch((error) => res.json(500).json({error: "Failed to created profile", details: error}))
-    });
+
+// Edits the user Profile
+router.put("/profile/:_id/edit", (req, res) => {
+    const { _id } = req.params;
+    const { username, email, password, createdRecipes } = req.body;
+    User.findByIdAndUpdate(_id,
+        { username, email, password, createdRecipes },
+        { new: true })
+        .then((updatedUser) => {
+            if(!updatedUser){
+                return res.status(404).json({ message: "User not found." });
+            }
+                res.json(updatedUser)
+        })
+        .catch((error)=> res.status(500).json({ error: "Failed to edit profile", details: error }))
+});
 
 
 module.exports = router;
