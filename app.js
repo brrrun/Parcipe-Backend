2e691e977acae40a8a1a7435fd2aa9c52cb6ffd9
brrrun <brunomsantosmendes@gmail.com>
2024-03-08 00:37:00 +0000

    new cors header

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const express = require("express");
 const app = express();
 
 const cors = require("cors");
-const allowedOrigins = [{"Access-Control-Allow-Origin":"*", origin:"https://parcipe.netlify.app"}];
+const allowedOrigins = [{"Access-Control-Allow-Origin":"*", "Access-Control-Allow-Origin":"https://parcipe.netlify.app"}];
 const corsOptions = {
   origin: allowedOrigins
 };
