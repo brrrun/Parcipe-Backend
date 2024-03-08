60242620d85d4ee3f60d12669a1c9e31dbdfe54f
brrrun <brunomsantosmendes@gmail.com>
2024-03-08 14:49:12 +0000

    reinstall cors

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ const allowedOrigins = ['*', 'https://parcipe.netlify.app'];
 const corsOptions = {
   allowedOrigins
 };
-/*app.use(cors());
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   next();
-})*/
+})
 // Increase the maximum size of the request body to 10MB
 const bodyParser = require('body-parser');
 app.use(bodyParser.json({ limit: '10mb' }));
